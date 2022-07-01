eb4641d42def1207daf3fa6ccb8a1109f4bb0001
Rahull78 <101934952+Rahull78@users.noreply.github.com>
2022-07-01 22:55:45 +0530

    Two new Files Added

---
+++ d.py
@@ -0,0 +1,9 @@
+a=int(input("Enter 1st number : "))
+b=int(input("Enter 2nd number : "))
+print("--------------------------")
+print("\t BEFORE SWAP")
+print("\t A=",a,"B=",b)
+a,b=b,a
+print("--------------------------")
+print("\t AFTER SWAP")
+print("\t A=",a,"B=",b)
---
+++ dd.py
@@ -0,0 +1,4 @@
+a=complex(input("ENTER 1ST NO : "))
+b=complex(input("ENTER 2ND NO : "))
+c=a+b
+print ("ANS = ",c)
